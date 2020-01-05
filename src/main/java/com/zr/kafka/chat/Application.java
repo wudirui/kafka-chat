e8678ff799c40acbdee9a9af33e563af3a7ee222
zhangrui <1570266831@qq.com>
2020-01-05 17:42:11 +0800

    初次提交

---
+++ src/main/java/com/zr/kafka/chat/Application.java
@@ -0,0 +1,13 @@
+package com.zr.kafka.chat;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication application = new SpringApplication();
+		application.run(Application.class,args);
+	}
+}
